52ded0b44800dc563df246b78cdbe11abf29836b
orikazu <orikazu.0824@i.softbank.jp>
2019-08-01 11:06:36 +0000

    resourcesを使って 実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get '/how_to_use' => 'home#how_to_use'
-  get '/about' => 'home#about'
-  get '/' => 'home#top'
-  get '/sample_contribution' => 'home#sample_contribution'
+  root to: 'themes#index'
+  resources :themes
+  # get 'themes/index'
+  # get 'themes/show'
+  # get '/how_to_use' => 'home#how_to_use'
+  # get '/about' => 'home#about'
+  # get '/' => 'home#top'
+  # get '/sample_contribution' => 'home#sample_contribution'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
