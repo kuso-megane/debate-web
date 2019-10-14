0ed3cfa86c8f9d85e7c624d13f2ecd7ebb77294b
orikazu <orikazu.0824@i.softbank.jp>
2019-10-14 08:48:29 +0000

    button機能

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -6,6 +6,12 @@ class OpinionsController < ApplicationController
     @agree_opinions = @theme.opinions.agree
     @disagree_opinions = @theme.opinions.disagree
     @neutral_opinions = @theme.opinions.neutral
+    
+    @agree_count = @agree_opinions.distinct.count(:user_id)
+    @disagree_count = @disagree_opinions.distinct.count(:user_id)
+    @neutral_count = @neutral_opinions.distinct.count(:user_id)
+    
+    @user_position = current_user.opinions.find_by(theme: @theme, content: nil)
   end  
   
   def new
@@ -13,10 +19,19 @@ class OpinionsController < ApplicationController
   end  
   
   def create
-    @opinion = Opinion.new(opinion_params)
+    @opinion = if params.dig(:opinion, :content).present?
+                 opinion = Opinion.new
+                 opinion.assign_attributes(opinion_params)
+                 opinion
+               else
+                 opinion = @theme.opinions.find_or_initialize_by(user: current_user, content: nil)
+                 opinion.assign_attributes(opinion_params)
+                 opinion
+               end
+    
     @opinion.user = current_user
     @opinion.theme = @theme
-    @opinion.opinion_type = @opinion.content.present? ? 'exist' : 'nothing'
+    @opinion.opinion_type = @opinion.content.nil? ? 'nothing' : 'exist'
     if @opinion.save
       redirect_to :action => "index"
     else
